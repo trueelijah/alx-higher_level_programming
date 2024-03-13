2597c1bf6283e20d73cc8771818ec1e1f4a82817
Elijah Adeloye <eaadeloye@gmail.com>
2024-03-13 19:41:20 +0100

    a script that creates a table second_table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- a script that creates a table second_table..
+CREATE TABLE second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table values (1, "John", 10);
+INSERT INTO second_table values (2, "Alex", 3);
+INSERT INTO second_table values (3, "Bob", 14);
+INSERT INTO second_table values (4, "George", 8);
