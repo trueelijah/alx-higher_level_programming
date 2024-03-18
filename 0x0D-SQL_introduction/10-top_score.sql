fa52c485cde53885fc8c8f5760d6af6ae68f5a2b
Elijah Adeloye <eaadeloye@gmail.com>
2024-03-18 16:32:33 +0100

    a script that lists all records of the table second_table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,5 @@
--- a script that lists all records of the table second_table..
-SELECT * FROM second_table ORDER BY score DESC;
+-- a script that lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
